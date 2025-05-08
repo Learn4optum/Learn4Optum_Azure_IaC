1f50ab6ac5851de2998357fbc15bd9c208a5e9ec
Learn4optum <learn4optum@gmail.com>
2025-05-08 15:43:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,8 @@
-# Provider configuration
 provider "azurerm" {
   features {}
 }
 
-# Resource group
 resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-# Output the resource group name
-output "resource_group_name" {
-  value = azurerm_resource_group.example.name
+  name     = "example-resource-group"
+  location = "East US"
 }
