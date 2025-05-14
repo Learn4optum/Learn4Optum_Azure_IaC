9ac51014e555614bf9905ad2f697b3e54280c680
mallik-raj <53124649+mallik-raj@users.noreply.github.com>
2025-05-14 23:38:26 +0530

    vnet revert

--- variables.tf
+++ variables.tf
@@ -1,42 +1,16 @@
 variable "resource_group_name" {
-  description = "Name of the Resource Group"
+  description = "The name of the resource group"
   type        = string
 }
 
-variable "location" {
-  description = "Azure Region"
+variable "resource_group_location" {
+  description = "The Azure location where the resource group will be created"
   type        = string
   default     = "East US"
 }
 
-variable "vnet_name" {
-  description = "Name of the Virtual Network"
-  type        = string
-}
-
-variable "address_space" {
-  description = "Address space for the VNet"
-  type        = list(string)
-}
-
-variable "dns_servers" {
-  description = "Optional custom DNS servers"
-  type        = list(string)
-  default     = []
-}
-
-variable "subnet_name" {
-  description = "Name of the Subnet"
-  type        = string
-}
-
-variable "subnet_prefixes" {
-  description = "Address prefixes for the subnet"
-  type        = list(string)
-}
-
-variable "environment" {
-  description = "Environment tag (e.g., dev, prod)"
+variable "subscription_id" {
+  description = "Azure Subscription ID"
   type        = string
 }
 
