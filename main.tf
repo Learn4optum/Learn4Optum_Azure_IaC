c6bc98767834111290b2940232cc422e988170bf
Learn4optum <learn4optum@gmail.com>
2025-05-14 21:04:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
 }
 
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
+
+
