fabaa3413a07a89902343f04bfeccc1d21ea6a6b
mallik-raj <53124649+mallik-raj@users.noreply.github.com>
2025-05-14 23:43:56 +0530

    vnet

--- main.tf
+++ main.tf
@@ -8,4 +8,21 @@ resource "azurerm_resource_group" "main" {
   location = var.resource_group_location
 }
 
+resource "azurerm_virtual_network" "main" {
+  name                = var.vnet_name
+  address_space       = var.address_space
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_servers         = var.dns_servers
 
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = var.subnet_prefixes
+}
