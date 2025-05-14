72f88eeac9f6f7f60549c24721b966ffc492eb10
Learn4optum <learn4optum@gmail.com>
2025-05-15 01:02:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,9 @@ resource "azurerm_resource_group" "main" {
 }
 
 resource "azurerm_virtual_network" "main" {
-  name                = var.vnet_name
-  address_space       = var.address_space
+  for_each            = var.vnet_name
+  name                = each.value_name
+  address_space       = each.value.address_space
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   dns_servers         = var.dns_servers
