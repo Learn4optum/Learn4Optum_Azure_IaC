1e01b26879f385bdf310d7fcf6ed1bb70871c733
Learn4optum <learn4optum@gmail.com>
2025-05-18 12:21:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,11 @@
-variable "resource_group_name" {
-  description = "The name of the resource group"
-  type        = string
+variable "resource_groups" {
+  type = map(object({
+    name     = string
+    location = string
+  }))
+  description = "Map of resource groups to create"
 }
+
 
 variable "resource_group_location" {
   description = "The Azure location where the resource group will be created"
