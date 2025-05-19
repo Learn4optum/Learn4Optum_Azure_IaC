ddea4ffb4cd6b67f73023e0ecbc257c30777d9f7
Learn4optum <learn4optum@gmail.com>
2025-05-19 23:35:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,14 +41,6 @@ resource "azurerm_storage_account" "storage" {
   }
 }
 
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "MES-DEV-RG"
-    storage_account_name = "mesdevstrg"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
 
 
 
