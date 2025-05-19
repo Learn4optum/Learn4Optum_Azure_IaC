e289d1bd4b1701b1970ee04dd870e7ed2f83ef85
Learn4optum <learn4optum@gmail.com>
2025-05-19 21:22:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,11 +30,4 @@ resource "azurerm_subnet" "subnets" {
 
 
 
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "MES-DEV-RG"
-    storage_account_name = "mesdevstrg"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
+
