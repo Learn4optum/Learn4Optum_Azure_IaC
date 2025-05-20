bd46ac5281b18e85a2ef1f5e01a1e161ecc145e9
mallik-raj <53124649+mallik-raj@users.noreply.github.com>
2025-05-20 15:40:35 +0530

    ve

--- main.tf
+++ main.tf
@@ -10,37 +10,6 @@ resource "azurerm_resource_group" "main" {
 }
 
 
-# Virtual Network
-resource "azurerm_virtual_network" "main" {
-  name                = var.vnet_name
-  address_space       = var.vnet_address_space
-  location            = var.location
-  resource_group_name  = azurerm_resource_group.main.name
-}
-
-# Subnets
-resource "azurerm_subnet" "subnets" {
-  for_each = { for subnet in var.subnets : subnet.name => subnet }
-
-  name                 = each.value.name
-  address_prefixes     = [each.value.address_prefix]
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-}
-
-resource "azurerm_storage_account" "storage" {
-  name                     = var.storage_account_name
-  resource_group_name  = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
-  account_tier             = var.account_tier
-  account_replication_type = var.replication_type
-  #allow_blob_public_access = false
-
-  tags = {
-    environment = var.environment
-  }
-}
-
 
 
 
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,29 +1,5 @@
-resource_group_name     = "MES-DEV-RG"
-location                = "East US"
-storage_account_name   = "mystorageac9849054345" # must be globally unique, lowercase, 3–24 chars
-account_tier           = "Standard"
-replication_type       = "LRS"
-environment            = "dev"
+resource_group_name     = "mes-dev-rg"
+location                = "centralus"
 subscription_id = "edd8e9bb-437a-457e-af2d-66f4db2fe514"
 
-vnet_name               = "vnet-uat"
-vnet_address_space      = ["10.0.0.0/16"]
 
-subnets = [
-  {
-    name           = "subnet-app"
-    address_prefix = "10.0.1.0/24"
-  },
-  {
-    name           = "subnet-db"
-    address_prefix = "10.0.2.0/24"
-  },
-  {
-    name           = "subnet-web"
-    address_prefix = "10.0.3.0/24"
-  },
-   {
-    name           = "subnet-webapp"
-    address_prefix = "10.0.4.0/24"
-  }
-]
