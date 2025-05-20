befe27668b84395ad85bd1366df7e02431c94239
mallik-raj <53124649+mallik-raj@users.noreply.github.com>
2025-05-20 17:06:21 +0530

    reg

--- backend.tf
+++ backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "MES-DEV-RG"
-    storage_account_name = "mesdevstrg"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
+  backend "azurerm"{}
 }
